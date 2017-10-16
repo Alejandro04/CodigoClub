66b64ae943d67ee92a2d0d8ce97fff02a4540163
Alejandro Roa <ingjesusroa@gmail.com>
2017-10-16 15:19:51 -0400

    agregar configuracion de piperclip para imagenes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,:omniauthable,
          :omniauth_providers => [:facebook]
   has_many :posts
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/minion.jpg"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  has_attached_file :cover, styles: { medium: "800x600>", thumb: "400x300>" }, default_url: "/images/:style/missing_cover.jpg"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   
   def self.from_omniauth(auth)
   	where(provider: auth[:provider], uid:auth[:uid]).first_or_create do |user|
